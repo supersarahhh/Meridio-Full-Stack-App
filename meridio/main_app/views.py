73e9cc2de4e5f95fc12a9cb3841b85158b682bc7
gollobc <cadegollob@gmail.com>
2023-07-17 13:56:24 -0700

    sample market display

--- meridio/main_app/views.py
+++ meridio/main_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 
-Posts = [
-    {'name': 'Desk', 'pic': 'assets/desk.jpeg', 'datetime': '7/10, 11:34AM', 'condition': 'used', 'description': '', 'price': 'Free' },
-    {'name': 'Samsung TV', 'pic': 'assets/TV.jpeg', 'datetime': '7/7, 1:23PM', 'condition': 'used', 'description': '', 'price': '$200' },
-    {'name': 'Couch', 'pic': 'assets/Couch.jpeg', 'datetime': '7/9, 3:41PM', 'condition': 'Mint', 'description': '', 'price': '$100' },
-    {'name': 'Refrigerator', 'pic': '', 'datetime': '7/11, 5:10PM', 'condition': 'Opened Box', 'description': '', 'price': '$125' },
-    {'name': 'Headphones', 'pic': '', 'datetime': '7/13', 'condition': 'used', 'description': '', 'price': '$50' },   
+posts = [
+    {'item': 'Desk', 'picture': 'assets/desk.jpeg', 'date': '7/10, 11:34AM', 'condition': 'used', 'description': '', 'price': '0' },
+    {'item': 'Samsung TV', 'picture': 'assets/TV.jpeg', 'date': '7/7, 1:23PM', 'condition': 'used', 'description': '', 'price': '200' },
+    {'item': 'Couch', 'picture': 'assets/Couch.jpeg', 'date': '7/9, 3:41PM', 'condition': 'Mint', 'description': '', 'price': '100' },
+    {'item': 'Refrigerator', 'picture': '', 'date': '7/11, 5:10PM', 'condition': 'Opened Box', 'description': '', 'price': '125' },
+    {'item': 'Headphones', 'picture': '', 'date': '7/13', 'condition': 'used', 'description': '', 'price': '50' },   
 ]
 # Create your views here.
 
@@ -18,5 +18,5 @@ def about(request):
 
 def market_index(request):
     return render(request, 'market/index.html', {
-        'items': items
+        'posts': posts
     })
