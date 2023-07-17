395ea758d5d6ae96da6bd57b72048a5898b6d352
Masato-88 <shigashi32@gmail.com>
2023-07-17 11:55:50 -0700

    Created model and views DIC

--- meridio/main_app/models.py
+++ meridio/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    pic = models.ImageField()
+    datetime = models.DateTimeField()
+    condition = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    price = models.DecimalField()
--- meridio/main_app/views.py
+++ meridio/main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+
+Posts = [
+    {'name': 'Desk', 'pic': 'assets/desk.jpeg', 'datetime': '7/10, 11:34AM', 'condition': 'used', 'description': '', 'price': 'Free' },
+    {'name': 'Samsung TV', 'pic': 'assets/TV.jpeg', 'datetime': '7/7, 1:23PM', 'condition': 'used', 'description': '', 'price': '$200' },
+    {'name': 'Couch', 'pic': 'assets/Couch.jpeg', 'datetime': '7/9, 3:41PM', 'condition': 'Mint', 'description': '', 'price': '$100' },
+    {'name': 'Refrigerator', 'pic': '', 'datetime': '7/11, 5:10PM', 'condition': 'Opened Box', 'description': '', 'price': '$125' },
+    {'name': 'Headphones', 'pic': '', 'datetime': '7/13', 'condition': 'used', 'description': '', 'price': '$50' },   
+]
 # Create your views here.
 
 def home(request):
